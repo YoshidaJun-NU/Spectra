62b1b864624a1f8bb9ffdf766c3d69033e24140c
YoshidaJun-NU <yoshida.jun@nihon-u.ac.jp>
2026-01-14 05:57:15 +0900

    0557

--- Pages/1_Spectra.py
+++ Pages/1_Spectra.py
@@ -151,7 +151,7 @@ def create_gnuplot_data(data_list):
 # ---------------------------------------------------------
 def main():
     st.set_page_config(page_title="Spectra Plotter", layout="wide")
-    st.title("Spectra Viewer 汎用")
+    st.title("Spectra Viewer ")
 
     if 'data_list' not in st.session_state:
         st.session_state['data_list'] = []
